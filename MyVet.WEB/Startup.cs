da9bdd2c205047836a75d022996608d985ef6771
gilciel.almeida <gilciel.almeida@hotmail.com>
2023-02-15 23:27:06 -0300

    Config acesso Banco de Dados

---
+++ MyVet.WEB/Startup.cs
@@ -0,0 +1,47 @@
+﻿using MyVet.WEB.Data;
+
+namespace MyVet.WEB
+{
+   public class Startup
+   {
+      public Startup(IConfiguration configuration)
+      {
+         Configuration= configuration;
+      }
+      public IConfiguration Configuration { get; }
+
+      public void ConfigureServices(IServiceCollection services)
+      {
+         // Add services to the container.
+         services.AddControllersWithViews();
+
+         services.AddDbContext<DataContext>(cfg =>
+         {
+           cfg.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+         });
+
+      }
+      public void Configure(WebApplication app, IWebHostEnvironment environment)
+      {
+         // Configure the HTTP request pipeline.
+         if (!app.Environment.IsDevelopment())
+         {
+            app.UseExceptionHandler("/Home/Error");
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
+         }
+
+         app.UseHttpsRedirection();
+         app.UseStaticFiles();
+
+         app.UseRouting();
+
+         app.UseAuthorization();
+
+         app.MapControllerRoute(
+             name: "default",
+             pattern: "{controller=Home}/{action=Index}/{id?}");
+
+      }
+   }
+}

87d73dcfb86d5939e6599bcf06044e7d227c6a55
gilciel.almeida <gilciel.almeida@hotmail.com>
2023-03-08 22:34:01 -0300

    Criacao Banco de Dados

--- MyVet.WEB/Program.cs
+++ MyVet.WEB/Program.cs
@@ -1,13 +1,34 @@
-using MyVet.WEB;
+using Microsoft.EntityFrameworkCore;
+using MyVet.WEB.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
-var startup = new Startup(builder.Configuration);
-startup.ConfigureServices(builder.Services);
-
+string connStr = builder.Configuration.GetConnectionString("DefaultConnections");
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<DataContext>
+     //(options => options.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Teste;Integrated Security = True; Connect Timeout = 30; Encrypt = False; TrustServerCertificate = False; ApplicationIntent = ReadWrite; MultiSubnetFailover = False"));
+     (options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnections")));
 
 var app = builder.Build();
-startup.Configure(app, app.Environment);
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+   app.UseExceptionHandler("/Home/Error");
+   // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+   app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
 app.Run();
